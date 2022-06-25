2d50e0b9b3a4174143c3d78c53c11e347d0ff7cd
eldesh <nephits@gmail.com>
2022-06-26 01:14:41 +0900

    Comment out

--- src/type.zig
+++ src/type.zig
@@ -79,27 +79,61 @@ comptime {
 
 fn impl_map(comptime T: type) type {
     assert(isIterator(T));
+    // conflict to existing function
+    assert(have_fun(T, "map") == null);
+    return void;
 
-    // if (!have_fun(T, "map")) {
-    //     const TypeInfo = std.builtin.TypeInfo;
-    //     var info: TypeInfo = @typeInfo(T);
-    //     var decls: [info.Struct.decls.len+1]TypeInfo.Declaration = undefined;
-    //     var i = 0;
-    //     while (i < info.Struct.decls.len) : (i += 1) {
-    //         decls[i] = info.Struct.decls[i];
+    // cannot override 'Self'
+    // var M = struct {
+    //     pub usingnamespace T;
+    //     pub const Self = @This();
+
+    //     pub fn map(self: *T.Self, f: anytype) iter.IterMap(T.Self, @TypeOf(f)) {
+    //         return iter.IterMap(T.Self, @TypeOf(f)).new(f, self);
     //     }
-    //     decls[i] = @typeInfo(struct {
-    //         pub fn map(self: *Self, f: F) IterMap(Self, F) {
-    //         }
-    //     }).Struct.decls[0];
+    // };
+    // M.Self = @TypeOf(M);
+    // return M;
+
+    // cannot reify declarations with @Type
+    // const TypeInfo = std.builtin.TypeInfo;
+    // var info: TypeInfo = @typeInfo(T);
+    // var decls: [info.Struct.decls.len + 1]TypeInfo.Declaration = undefined;
+    // var i = 0;
+    // while (i < info.Struct.decls.len) : (i += 1) {
+    //     decls[i] = info.Struct.decls[i];
     // }
+    // decls[i] = @typeInfo(struct {
+    //     pub fn map(self: *@This(), f: anytype) iter.IterMap(@This(), @TypeOf(f)) {
+    //         return iter.IterMap(@This(), @TypeOf(f)).new(self, f);
+    //     }
+    // }).Struct.decls[0];
+    // @compileLog("info.Struct.decls:", info.Struct.decls);
     // var ty = @Type(info);
     // info.Struct.decls[0].data = .{ .Type = ty };
     // return @Type(ty);
 }
 
-test "impl_map" {
-    const SliceIterMap = impl_map(SliceIter(u32));
-    assert(isIterator(SliceIterMap));
-    _ = SliceIterMap;
-}
+// test "impl_map" {
+//     const SliceIterMap = impl_map(SliceIter(u32));
+//     assert(isIterator(SliceIterMap));
+//
+//     const arr = [_]u32{ 1, 2, 3 };
+//     var slice = SliceIter(u32).new(arr[0..]);
+//     try testing.expect(slice.next().? == 1);
+//     try testing.expect(slice.next().? == 2);
+//     try testing.expect(slice.next().? == 3);
+//     try testing.expect(slice.next() == null);
+//
+//     const Double = struct {
+//         pub fn apply(x: u32) u32 {
+//             return x * 2;
+//         }
+//     };
+//
+//     var map = SliceIterMap.new(arr[0..]).map(Double.apply);
+//     try testing.expect(map.next().? == 2);
+//     try testing.expect(map.next().? == 4);
+//     try testing.expect(map.next().? == 6);
+//     try testing.expect(map.next() == null);
+// }
