9fbe010c6d3db9acb53be882020e6f8675d3b81f
eldesh <nephits@gmail.com>
2022-08-20 00:39:10 +0900

    Add: bounded_array from iter

--- src/from_iter.zig
+++ src/from_iter.zig
@@ -9,6 +9,7 @@ const assert = std.debug.assert;
 
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+const BoundedArray = std.BoundedArray;
 
 /// Create a slice consisting of the elements enumrated from `iter`.
 pub fn slice_from_iter(alloc: Allocator, iter: anytype) Allocator.Error![]@TypeOf(iter).Item {
@@ -44,6 +45,31 @@ test "array_list_from_iter" {
 
     try testing.expectEqual(@as(usize, 10), arr.items.len);
     for (arr.items) |val, i| {
+        try testing.expectEqual(i, val);
+    }
+}
+
+pub const BoundedArrayError = error{Overflow};
+
+/// Create an BoundedArray consisting of the elements enumrated from `iter`.
+pub fn bounded_array_from_iter(comptime N: usize, iter: anytype) BoundedArrayError!BoundedArray(@TypeOf(iter).Item, N) {
+    const Iter = @TypeOf(iter);
+    comptime assert(meta.isIterator(Iter));
+
+    var xs = try BoundedArray(Iter.Item, N).init(0);
+    var it = iter;
+    while (it.next()) |val| {
+        try xs.append(val);
+    }
+    return xs;
+}
+
+test "bounded_array_from_iter" {
+    var rng = range.range(@as(u32, 0), 10, 1);
+    const arr = try bounded_array_from_iter(10, rng);
+
+    try testing.expectEqual(@as(usize, 10), arr.constSlice().len);
+    for (arr.constSlice()) |val, i| {
         try testing.expectEqual(i, val);
     }
 }
