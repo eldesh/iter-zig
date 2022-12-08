66bc9ce31b20956b18c5b00e3bd8c5f116c59cff
eldesh <nephits@gmail.com>
2022-12-08 05:49:36 -0800

    Fix: switch have_fun on Zig version

--- src/meta.zig
+++ src/meta.zig
@@ -294,11 +294,42 @@ fn hasDecl(comptime T: type, comptime name: []const u8) bool {
 
 pub fn have_fun(comptime T: type, comptime name: []const u8) ?type {
     comptime {
-        if (!std.meta.trait.isContainer(T))
+        if (newer_zig091) {
+            if (!std.meta.trait.isContainer(T))
+                return null;
+            if (!hasDecl(T, name))
+                return null;
+            return @as(?type, @TypeOf(@field(T, name)));
+        } else {
+            switch (@typeInfo(T)) {
+                .Struct => |Struct| {
+                    for (Struct.decls) |decl| {
+                        if (std.mem.eql(u8, decl.name, name))
+                            return @TypeOf(@field(T, name));
+                    }
+                },
+                .Union => |Union| {
+                    for (Union.decls) |decl| {
+                        if (std.mem.eql(u8, decl.name, name))
+                            return @TypeOf(@field(T, name));
+                    }
+                },
+                .Enum => |Enum| {
+                    for (Enum.decls) |decl| {
+                        if (std.mem.eql(u8, decl.name, name))
+                            return @TypeOf(@field(T, name));
+                    }
+                },
+                .Opaque => |Opaque| {
+                    for (Opaque.decls) |decl| {
+                        if (std.mem.eql(u8, decl.name, name))
+                            return @TypeOf(@field(T, name));
+                    }
+                },
+                else => {},
+            }
             return null;
-        if (!hasDecl(T, name))
-            return null;
-        return @as(?type, @TypeOf(@field(T, name)));
+        }
     }
 }
 
